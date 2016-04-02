39e407ac336294e9659543fb7140dd60f1ed8a23
Simon VANDPUTTE <simon.vandeputte@etudiant.univ-lille1.fr>
2016-04-02 15:40:15 +0200

    sans oublier de personnage

---
+++ src/Personnage.java
@@ -0,0 +1,47 @@
+/**
+ * Class pour la gestion des personnages
+ * @author Simon, Valentin et Emeline
+ *
+ */
+public abstract class Personnage {
+
+	protected Ile ile;
+	protected int energie; 
+	protected int equipe; 
+	protected int PosX;
+	protected int PosY;
+	protected boolean clé;
+	protected boolean trésor;
+
+	public Personnage (int equipe, Ile ile){
+		this.equipe = equipe;
+		if(equipe == 1){
+			for(int i=0;i<ile.tableau.length;i++){
+				for(int j=0;j<ile.tableau.length;j++){
+					if(ile.tableau[i][j].getNb()==3){
+						PosX = i+1;
+						PosY = j;
+					}
+				}
+			}
+		} else if (equipe ==2){
+			for(int i=0;i<ile.tableau.length;i++){
+				for(int j=0;j<ile.tableau.length;j++){
+					if(ile.tableau[i][j].getNb()==4){
+						PosX = i;
+						PosY = j+1;
+					}
+				}
+			}
+		}
+		this.ile = ile;
+	}
+
+
+	public void obtiensTrésor(){
+		trésor = true;
+	}
+	public void obtiensClé(){
+		clé = true;
+	}
+}
